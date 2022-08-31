3b6609c3f972dc85cd58836fd2184181ee7e7c0b
johanmkr <johan.m.kroken@gmail.com>
2022-08-31 11:56:05 +0200

    Started task 7, but not sure about what to solve

--- project1/code/main.cpp
+++ project1/code/main.cpp
@@ -13,9 +13,9 @@ double u(double x){
 
 
 
-
 int main(){
 
+    // task 2
     std::string filename="x_u.txt";
     std::ofstream ofile;
     ofile.open(filename);
@@ -28,14 +28,14 @@ int main(){
     double x_min = 0; 
     double x_max = 1;
     int n_steps = 100;
+    int n_points = n_steps+1;
     double h = (x_max - x_min) / n_steps;
 
-    std::vector<double> x(n_steps + 1);
-    std::vector<double> v(n_steps + 1);
+    std::vector<double> x(n_points);
+    std::vector<double> v(n_points);
 
     x[0] = x_min;
     v[0] = u(x[0]);
-
 
     for(int i=0; i<n_steps; i++){
         x[i+1] = x[i] + h;
@@ -50,6 +50,20 @@ int main(){
 
 
     ofile.close();
+
+
+    // task 7 THIS IS PROBABLY WRONG
+    std::vector<double> a(n_points, 2);
+    std::vector<double> b(n_points-1, -1);
+    std::vector<double> c(n_points-1, -1);
+    std::vector<double> g(n_points);
+
+    for(int i=1; i<n_points-1; i++){
+        g[i] = a[i-1]*v[i-1]+ b[i]*v[i] + c[i]*v[i+1];
+    }
+    
+    //boundary points
+    g[0]= 
 
     return 0;
 
