4fd397b49717cea4b2f500544e9eff884f4a2e25
nannabryne <nannabryne@outlook.com>
2022-09-06 11:13:22 +0200

    Python script for checking if the results are good

---
+++ project1/testing_nanna/test_plots.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+n_steps = [10, 100, 1000]
+path = "folder/"
+
+fig, ax = plt.subplots()
+for n in n_steps:
+    file = f'num_sol_{n}steps.txt'
+    x, v = np.loadtxt(path + file, unpack=True)
+    ax.plot(x, v, ls='--', label=r'$n_{steps}=%i$'%n)
+
+u = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
+ax.plot(x, u, lw=1.2, c='k', label=r'$u(x)$', alpha=0.6)
+
+ax.legend()
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$v(x)$')
+plt.show()
