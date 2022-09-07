9937d2b4b6bfb3072432c8973a5983c044f4cce3
johanmkr <johan.m.kroken@gmail.com>
2022-09-07 11:53:18 +0200

    Added error calculations, although this can be done better

--- project1/code/main.cpp
+++ project1/code/main.cpp
@@ -107,9 +107,12 @@ const double x_max = 1;
 const double v0 = 0;
 const double v1 = 0;
 
+
+// declare functions
 int problem2(int n_steps);
 int problem7(int n_steps);
 int problem9(int n_steps);
+int problem8(int n_steps, bool absolute_error, bool maximum_error);
 
 int main(){
 
@@ -124,6 +127,14 @@ int main(){
     problem9(10);
     problem9(100);
     problem9(1000);
+
+    problem8(10, true, false);
+    problem8(100, true, false);
+    problem8(1000, true, false);
+
+    problem8(10, false, false);
+    problem8(100, false, false);
+    problem8(1000, false, false);
 
     auto stop_time = std::chrono::high_resolution_clock::now();
 
@@ -225,5 +236,65 @@ int problem9(int n_steps){
 
     writeto_file(x, v, "special_num_sol_" + std::to_string(n_steps) + "steps");
 
+    return 0;
+}
+
+int problem8(int n_steps, bool absolute_error, bool maximum_error){
+    int n_points = n_steps+1;
+    double h = (x_max - x_min) / n_steps;
+    int m = n_points - 2;
+    std::vector<double> x(n_points);
+    x[0] = x_min;
+    x[n_points-1] = x_max;
+    std::vector<double> g(m);
+   
+    for(int i=1; i<=m; i++){
+        x[i] = x_min + i*h;
+        g[i-1] = std::pow(h,2) * f(x[i]); 
+    }
+    g[0] += v0;
+    g[n_points-1] += v1;
+
+    std::vector<double> a(m, -1);
+    std::vector<double> b(m, 2);
+    std::vector<double> c(m, -1);
+    
+    std::vector<double> vstar = generalThomas(a, b, c, g);
+    
+    // Include endpoints
+
+    std::vector<double> v(n_points);
+    v[0] = v0;
+    v[n_points-1] = v1;
+    for(int i=1; i<=m; i++){
+        v[i] = vstar[i-1];
+    }
+    
+    std::vector<double> u_exact(n_points);
+    for(int i=0; i<=n_steps; i++){
+        u_exact[i] = u(x[i]);
+    }
+
+    if (absolute_error==true && maximum_error==false){
+        std::vector<double> absolute_error(n_points);
+        absolute_error[0] = 0;
+        absolute_error[n_points-1] = 0;
+        for(int i=1; i<n_steps; i++){
+            double abs_err = std::abs(u(x[i])-v[i]);
+            absolute_error[i] = std::log10(abs_err);
+        }
+        writeto_file(x, absolute_error, "absolute_error" + std::to_string(n_steps) + "steps");
+    }
+    else if (absolute_error==false && maximum_error==false){
+        std::vector<double> relative_error(n_points);
+        relative_error[0] = 0;
+        relative_error[n_points-1] = 0;
+        for(int i=1; i<n_steps; i++){
+            double rel_err = std::abs((u(x[i])-v[i])/u(x[i]));
+            relative_error[i] = std::log10(rel_err);
+        }
+        writeto_file(x, relative_error, "relative_error" + std::to_string(n_steps) + "steps");
+    }
+    // MAXIMUM RELATIVE ERROR FOR n_steps up to 10^7.
     return 0;
 }
