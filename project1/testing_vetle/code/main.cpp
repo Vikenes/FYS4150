e654618f7886bc56098dc74efe9ef504c4aad482
Vetle Vikenes <vetle.vkns@gmail.com>
2022-09-10 11:27:16 +0200

    Timing problem implemented in new main file

--- project1/testing_vetle/code/main.cpp
+++ project1/testing_vetle/code/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <chrono>
 
 #include "utils.hpp"
 #include "algorithms.hpp"
@@ -49,7 +50,7 @@ std::vector<double> g_array(int n_steps){
 
 
 
-std::vector<double> compute_generalThomas(int n_steps, bool write=false){
+std::vector<double> compute_generalThomas(int n_steps, bool write=false, int timing=0){
     int m = n_steps - 1;
     std::vector<double> a(m, -1);
     std::vector<double> b(m, 2);
@@ -58,25 +59,55 @@ std::vector<double> compute_generalThomas(int n_steps, bool write=false){
     std::vector<double> x=x_array(n_steps);
     std::vector<double> g=g_array(n_steps);
 
-    std::vector<double> v = generalThomas(a, b, c, g);
+    if (timing!=0){
+        std::vector<double> tot_time(1);
 
-    if (write==true){
-        write_to_file(x, v, "GT_" + std::to_string(n_steps) + "steps");
+        auto t1 = std::chrono::high_resolution_clock::now();
+        for(int j=0; j<timing; j++){
+            std::vector<double> v_time = generalThomas(a, b, c, g);
+        }
+        auto t2 = std::chrono::high_resolution_clock::now();
+        
+        tot_time[0] = std::chrono::duration<double>(t2 - t1).count();//number_of_runs_per_step;
+        return tot_time;
     }
-    return v;
+
+    else{
+        std::vector<double> v = generalThomas(a, b, c, g);
+        if (write==true){
+            write_to_file(x, v, "GT_" + std::to_string(n_steps) + "steps");
+        }
+        return v;
+    }
+
 } 
 
 
-void compute_specialThomas(int n_steps, bool write=false){
-    int m = n_steps - 1;
+std::vector<double> compute_specialThomas(int n_steps, bool write=false, int timing=0){
     
-    std::vector<double> x=x_array(n_steps);
     std::vector<double> g=g_array(n_steps);
 
-    std::vector<double> v_star = specialThomas(g);
+    if (timing!=0){
+        std::vector<double> tot_time(1);
 
+        auto t1 = std::chrono::high_resolution_clock::now();
+        for(int j=0; j<timing; j++){
+            std::vector<double> v_time = specialThomas(g);
+        }
+        auto t2 = std::chrono::high_resolution_clock::now();
+        
+        tot_time[0] = std::chrono::duration<double>(t2 - t1).count();//number_of_runs_per_step;
+        return tot_time;
+    }
+
+    else{   
+    std::vector<double> v_star = specialThomas(g);
     if (write==true){
+        std::vector<double> x=x_array(n_steps);
         write_to_file(x, v_star, "ST_" + std::to_string(n_steps) + "steps");
+    }
+    return v_star;
+    
     }
 
 }
@@ -138,17 +169,43 @@ int max_rel_err(int number_of_n_steps){
         std::vector<double> rel_err = relative_error(n_steps);
         max_error_n_step[i-1] = *std::max_element(rel_err.begin(), rel_err.end());
     }
-    write_to_file(n_steps_array, max_error_n_step, "test_max_relative_error");
+    write_to_file(n_steps_array, max_error_n_step, "max_rel_error");
     return 0;
 }
 
+
+void timing(){
+    int n_step_sizes = 6;
+    int n_simulations = 250;
+    std::vector<double> list_of_n_steps(n_step_sizes); // double for writing to file 
+    std::vector<double> general_Thomas_timed(n_step_sizes);
+    std::vector<double> special_Thomas_timed(n_step_sizes);
+    for(int i=0; i<n_step_sizes; i++){
+        list_of_n_steps[i] = std::pow(10,i+1);
+    }
+
+    for(int i=0; i<n_step_sizes; i++){
+        int n_steps = list_of_n_steps[i];
+
+        general_Thomas_timed[i] = compute_generalThomas(n_steps, false, n_simulations)[0]/n_simulations;
+        special_Thomas_timed[i] = compute_specialThomas(n_steps, false, n_simulations)[0]/n_simulations;
+    }
+    
+    write_to_file(list_of_n_steps, general_Thomas_timed, "general_thomas_timed");
+    write_to_file(list_of_n_steps, special_Thomas_timed, "special_thomas_timed");
+
+    return;
+}
+
+
 int main(){
+
     
     // write_u(50, "analytical_xu_50");
 
     // std::vector<double> a()
     // compute_generalThomas(50);
-    // compute_specialThomas(50);
+    // compute_specialThomas(100);
     // absolute_error(10);
     // absolute_error(100);
     // absolute_error(1000);
@@ -156,7 +213,8 @@ int main(){
     // relative_error(100);
     // relative_error(1000);
 
-    max_rel_err(7);
+    // max_rel_err(7);
+    // timing();
 
     return 0;
 
