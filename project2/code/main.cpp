68c272c39ffc828c7903499d66749e1ed185f0c6
nannabryne <nannabryne@outlook.com>
2022-09-25 18:12:25 +0200

    Now main.cpp has code for p2 and p3. The function 'test_for_babycase()' is ready for testing the jacobi algorithm

--- project2/code/main.cpp
+++ project2/code/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <armadillo>
 #include <cmath>
+#include <assert.h>
+
 
 const double pi = 3.14159265359;
 
@@ -66,19 +68,76 @@ arma::mat create_tridiagonal(int N, double a, double d, double e){
     return create_tridiagonal(a_vec, d_vec, e_vec);
 }
 
-// Create a symmetric tridiagonal matrix tridiag(a,d,a) of size n*n
-// from scalar input a and d.
+// Create a symmetric tridiagonal matrix tridiag(a,d,a) of size N*N from scalar input a and d.
 arma::mat create_symmetric_tridiagonal(int N, double a, double d){
     // Call create_tridiagonal and return the result
     return create_tridiagonal(N, a, d, a);
 }
 
 
+double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){
+    // Get size of the matrix A. Use e.g. A.n_rows, see the Armadillo documentation
+    int N = A.n_rows;
 
-int problem2(){
-    // lack of a better name
+    // Consistency checks:
+    assert(A.is_square());   
+    assert(A.is_symmetric()); 
+
+    // Initialize references k and l to the first off-diagonal element of A
+    k = 0;
+    l = 1;
+
+    // Initialize a double variable 'maxval' to A(k,l). We'll use this variable
+    // to keep track of the largest off-diag element.
+    double maxval = A(k, l);
+
+    // Loop through all elements in the upper triangle of A (not including the diagonal)
+    // When encountering a matrix element with larger absolute value than the current value of maxval
+    // update k, l and max accordingly.
+    for(int i=0; i<N-1; i++){
+        for(int j=i+1; j<N; j++){
+            double element = std::abs(A(i, j));
+            if(element > maxval){
+                maxval = element;
+                k = i;
+                l = j;
+            }
+        }
+    }
+
+    // Return maxval 
+    return maxval;
+}
 
 
+// Test funtion for max_offdiag_symmetric()
+int test_max_offdiag_symmetric(){
+    // Creating test matrix
+    arma::mat A = arma::mat(4, 4, arma::fill::eye);
+    A(0,3) = 0.5;
+    A(1,2) = -0.7;
+    A(2,1) = -0.7;
+    A(3,0) = 0.5;
+    
+    // Calling the function to test
+    int k;
+    int l;
+    double maxval = max_offdiag_symmetric(A, k, l);
+
+    // Asserting expected vs. computet values
+    assert(maxval == 0.7);
+    assert(k == 1);
+    assert(l == 2);
+
+    return 0;
+}
+
+
+// Check results for the 6x6 tridiagonal symmetric matrix A with signature (a,d,a)
+//      (Comparing computed eigenvalues and -vectors using Armadillo- and/or Jacobi-solver.)
+int check_for_babycase(std::string which="arma"){
+
+    // Matrix A for N = 6
     int N = 6;
     double h = step_size(N+1);
     double h2 = std::pow(h, 2);
@@ -86,39 +145,66 @@ int problem2(){
     double d = 2/h2;
     arma::mat A = create_symmetric_tridiagonal(N, a, d);
 
-    // Find eigenvals, eigenvecs using armadillo
-    arma::vec eigval; 
-    arma::mat eigvec;
-    arma::eig_sym(eigval, eigvec, A);
-
-
-    // Find eigenvals, eigenvecs using analytical expressions
-
-    arma::vec lambda = arma::vec(N);
-    arma::mat v = arma::mat(N, N);
-    
+    // Eigenvectors, eigenvalues with analytical expressions
+    arma::vec eigval = arma::vec(N);
+    arma::mat Eigvec = arma::mat(N, N);
     for(int i=1; i<=N; i++){
-        lambda(i-1) = d + 2*a*std::cos(i*pi/(N+1));
+        eigval(i-1) = d + 2*a*std::cos(i*pi/(N+1));
         for(int j=1; j<=N; j++){
-            v(j-1,i-1) = std::sin(j * i*pi/(N+1));
+            Eigvec(j-1,i-1) = std::sin(j * i*pi/(N+1));
         }
     }
-    arma::mat vnorm = arma::normalise(v);
-    std::cout << lambda-eigval << std::endl;
-    std::cout << vnorm-eigvec << std::endl;
+    arma::mat eigvec = arma::normalise(Eigvec);
 
+
+    arma::vec eigval_test; 
+    arma::mat eigvec_test;
+    // Check with Armadillo
+    if(which == "arma"){
+        arma::eig_sym(eigval_test, eigvec_test, A);   
+        std::cout << "Checking if we use arma::eig_sym correctly." << std::endl;
+    }
+    // Check with Jacobi algorithm
+    else if(which == "jacobi"){
+        int p; // do something else
+        std::cout << "Checking if we implement Jacobi rotation method correctly." << std::endl;
+    }
+    else if(which == "both"){
+        int p; // do both
+        std::cout << "Checking if we use arma::eig_sym and implement Jacobi rotation method correctly." << std::endl;
+    }
+    else{
+        std::cout << "Provide valid argument" << std::endl;
+    }
+
+    // Check if they are equal
+    arma::vec vals = eigval_test/eigval;
+    arma::mat vecs = eigvec_test/eigvec;
+    vecs = arma::abs(vecs);
+
+    double tol = 0.0000001;
+    bool is_vecs = arma::approx_equal(vecs, arma::mat(N,N).fill(1.), "absdiff",  tol);
+    bool is_vals = arma::approx_equal(vals, arma::vec(N).fill(1.),   "absdiff", tol);
+
+    assert(is_vecs);
+    assert(is_vals);
 
     return 0;
-}   
+
+}
+
+
 
 
 int main(){
 
     // PROBLEM 2
+    check_for_babycase();
 
-    problem2();
+    // PROBLEM 3
+    test_max_offdiag_symmetric();
 
-
+    
 
     return 0;
 }
