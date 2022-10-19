7a9e7d9b3a68a5fa3fa50095dea5dd952bf3b247
Vetle Vikenes <vetle.vkns@gmail.com>
2022-10-19 13:04:43 +0200

    created analysis file

---
+++ project3/code/plot/analysis.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib import cm 
+import seaborn as sns 
+import pandas as pd 
+import plot 
+
+def compare_z_analytical():
+    return None 
