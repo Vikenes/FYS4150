7393d2e2400c46e1af158aea8a65fc39db41e942
johanmkr <johan.m.kroken@gmail.com>
2022-10-19 14:58:46 +0200

    trying a new 3-class approach for simpler code

---
+++ project3/code/include/Simulate.hpp
@@ -0,0 +1,17 @@
+#ifndef __Simulate_hpp__
+#define __Simulate_hpp__
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <armadillo>
+#include <assert.h>
+#include <Particle.hpp>
+#include <PenningTrap.hpp>
+
+
+
+
+
+
+#endif
