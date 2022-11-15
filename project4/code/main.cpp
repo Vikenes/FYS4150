285f143f82027847309267832ab2991346d9a6b3
Vetle Vikenes <vetle.vkns@gmail.com>
2022-11-15 18:17:55 +0100

    testing 2x2 analytical works, in early stage

--- project4/code/main.cpp
+++ project4/code/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <random> 
 #include "utils.hpp"
 
 /**
@@ -34,7 +35,9 @@ std::vector<double> DeltaE(double beta){
 }
 
 void initial_configuration(arma::mat& Lattice, double& E, double& M){
+
     int n_spins = Lattice.n_rows;
+    
     for(int x=0; x<n_spins; x++){
         for(int y=0; y<n_spins; y++){
             M += (double) Lattice(x,y);
@@ -44,7 +47,76 @@ void initial_configuration(arma::mat& Lattice, double& E, double& M){
     }
 }
 
-// void MC_cycle(arma::mat& Lattice, )
+void MC_cycle(arma::mat& Lattice, const std::vector<double> dE, double&E, double& M, unsigned int seed){
+    std::mt19937_64 generator(seed);
+    int n_spins = Lattice.n_rows;
+    // std::cout << "E_before: " << E;
+    for(int x=0; x<n_spins; x++){
+        for(int y=0; y<n_spins; y++){
+            int ix = (unsigned int) generator() % n_spins;
+            int iy = (unsigned int) generator() % n_spins;
+            int deltaE = 2*Lattice(ix,iy) * 
+                    (Lattice(ix, PBC(iy+1, n_spins)) + 
+                    Lattice(PBC(ix+1, n_spins), iy) + 
+                    Lattice(ix, PBC(iy-1, n_spins)) + 
+                    Lattice(PBC(ix-1, n_spins), iy));
+            int dE_idx = deltaE/4 + 2; 
+            if(deltaE <= 0){
+                Lattice(ix,iy) *= -1;
+                M += (double) 2*Lattice(ix,iy);
+                E += (double) deltaE;}
+            else{
+                double w = dE[dE_idx];
+                std::uniform_real_distribution<double> r(0,1);
+                if(r(generator) <= w){
+                    Lattice(ix,iy) *= -1;
+                    M += (double) 2*Lattice(ix,iy);
+                    E += (double) deltaE;
+                }
+            }
+        }
+    }
+}
+
+
+void test_analytical(int MC_cycles){
+    std::mt19937 generator(690);
+    arma::mat Lattice = initialize(2);
+    std::vector<double> dE = DeltaE(1);
+    std::vector<double> average(4, 0.0);
+
+    // int MC_cycles = 10000;
+
+    double E = 0;
+    double M = 0;
+
+    initial_configuration(Lattice, E, M);
+
+    std::string path = "../output/data/";
+    std::string name = "test_analytical_" + std::to_string(MC_cycles) + "_cycles";
+    std::string fname = path + name + ".txt";
+
+    std::ofstream avg_values;  
+    avg_values.open(fname.c_str()); // , std::ios_base::app | std::ios_base::in);
+    avg_values << "step, E, E2, absM, M2" << std::endl;
+
+    for(int cycle=1; cycle <= MC_cycles; cycle++){
+        MC_cycle(Lattice, dE, E, M, generator());
+        average[0] += E; average[1] += E*E;
+        average[2] += abs(M); average[3] += M*M;
+        // write_averages(average, cycle, avg_values);
+        // write_averages(average, cycle, avg_values)
+        avg_values << cycle << ", ";
+        avg_values << scientific_format(average[0]) << ", "
+                    << scientific_format(average[1]) << ", "
+                    << scientific_format(average[2]) << ", "
+                    << scientific_format(average[3]) << std::endl;
+    }
+
+    avg_values.close();
+    
+    std::cout << "Completed " << MC_cycles << " Monte Carlo cycles" << std::endl;
+}
 
 
 
@@ -52,9 +124,8 @@ int main(){
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    arma::mat Lattice = initialize(5);
-    arma::vec dE = DeltaE(1);
-
+    test_analytical(10000);
+    
     auto stop_time = std::chrono::high_resolution_clock::now();
 
     double run_time = std::chrono::duration<double>(stop_time-start_time).count();
