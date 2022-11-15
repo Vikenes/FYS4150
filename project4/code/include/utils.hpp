b78c587d826431108c25d579ed332f6866eb7051
Vetle Vikenes <vetle.vkns@gmail.com>
2022-11-15 18:17:02 +0100

    tried stuff, didn't work, undid

--- project4/code/include/utils.hpp
+++ project4/code/include/utils.hpp
@@ -22,18 +22,7 @@ int write_to_file(std::vector<double> a, std::vector<double> b, std::string file
 int write_to_file(std::vector<double> a, std::vector<int> b, std::string filename, int width=15);
 
 
-//  Constans for this project in units of micrometer, microseconds, atomic mass unit and elementary charge
 
-extern double k_e; //  Coulomb constant
-extern double T;   //  Tesla
-extern double V;   //  Volt
-extern double B0;  //  Magnetic field configuration in Penning trap
-extern double V0;  //  Electric potential configuration in Penning trap
-extern double d;   //  Size measure of Penning trap
-extern double Vdr;    //  Ratio of V0/d^2
-
-extern double q_Ca;
-extern double m_Ca;
 
 
 #endif
