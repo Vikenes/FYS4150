0b9e9f6b60d5bd4a2aa590bc26433be4953f1d01
Vetle Vikenes <vetle.vkns@gmail.com>
2022-11-23 07:20:31 +0100

    analytical fixed

--- project4/code/analytical.cpp
+++ project4/code/analytical.cpp
@@ -12,59 +12,47 @@ int main(int argc, char* argv[]){
         std::string executable_name = argv[0];
         std::cerr << "Error: Wrong number of input arguments. Got " << argc << std::endl;
         std::cerr << "Usage: " << executable_name << 
-                    " <Num. of MC cycles> <Save every N step> <T> " << std::endl;
+                    " <log10 of first sample> <log10 of last sample> <T> " << std::endl;
         
         return 1;
     }
 
-    int N_cycles = atoi(argv[1]);
-    int N_save = atoi(argv[2]);
+    int Nlog_min = atoi(argv[1]);
+    int Nlog_max = atoi(argv[2]);
     double T = atof(argv[3]);
 
+    int L = 2;
+    int N_eq = 0;
+    int N_meas = Nlog_max - Nlog_min + 1;
+    std::cout << "N0=" << Nlog_min << ", N1=" << Nlog_max << ", steps=" << N_meas << std::endl; 
+    arma::mat results(N_meas, 7, arma::fill::zeros);
+    
+    unsigned int base_seed = 690;
+
+   
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    std::mt19937 generator(690);
-    arma::mat Lattice = make_Lattice(2);
-    std::vector<double> dE = DeltaE(T);
+    int count = 0;
 
-    std::vector<double> average(4, 0.0);
-   
-    double E = 0;
-    double M = 0;
+    for(int i=Nlog_min; i <= Nlog_max; i++){
+        int N_cycles = std::pow(10,i);
+        std::cout << "count=" << count << ", N=" << N_cycles << std::endl;
+        arma::rowvec average = run_MC(L, T, N_cycles, N_eq, base_seed);
+        results.row(count) = average;
+        results(count, 5) = N_cycles;
+        count += 1;
+    }
 
-    compute_E_and_M(Lattice, E, M);
+    results.shed_col(6);
 
     std::string path = "../output/data/";
-    std::string name = "analytical_" + std::to_string(N_cycles) + "_cycles_T" + float_to_string(T);
-    std::string fname = path + name + ".txt";
-    std::ofstream avg_values;
-    avg_values.open(fname.c_str());
+    std::string name = "anal_Nsamples" + std::to_string(N_meas) + "_T" + float_to_string(T);
+    std::string ofilename = path + name + ".csv";
+    std::cout << "Saving to: " << ofilename << std::endl;
     
-    std::cout << "Saving to: " << fname << std::endl;
-
-    avg_values << "step, E, E2, absM, M2" << std::endl;
-
-    for(int cycle=1; cycle <= N_cycles; cycle++){
-        MC_cycle(Lattice, dE, E, M, generator());
-        average[0] += E; average[1] += E*E;
-        average[2] += abs(M); average[3] += M*M;
-
-        if(cycle%N_save == 0){
-            avg_values << cycle << ", ";
-            avg_values  << scientific_format(average[0]/cycle) << ", "
-                        << scientific_format(average[1]/cycle) << ", "
-                        << scientific_format(average[2]/cycle) << ", "
-                        << scientific_format(average[3]/cycle) << std::endl;
-        }
-    }
-    std::cout << "Finished saving" << std::endl;
-    avg_values.close();
-
-
+    results.save(ofilename, arma::csv_ascii);
     
-
     auto stop_time = std::chrono::high_resolution_clock::now();
-
     double run_time = std::chrono::duration<double>(stop_time-start_time).count();
 
     std::cout << "Running time: " << run_time << " s (" << run_time/60.0 << " min)" << std::endl;
