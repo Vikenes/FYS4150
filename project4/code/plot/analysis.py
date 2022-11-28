b82034e6a08d8636c7e590a989eae9adad44aea9
johanmkr <johan.m.kroken@gmail.com>
2022-11-28 15:23:16 +0100

    Sør-Korea 2 - 2 Ghana wow for ein kamp

--- project4/code/plot/analysis.py
+++ project4/code/plot/analysis.py
@@ -1,32 +1,108 @@
-from cProfile import label
 import numpy as np 
 import matplotlib.pyplot as plt 
-from matplotlib import cm 
-from IPython import embed
-import seaborn as sns 
 import pandas as pd 
 from scipy.interpolate import CubicSpline, UnivariateSpline
 from scipy.stats import linregress
 import os 
-import plot 
+
+plt.style.use("seaborn")
 
 #   Paths
 here = os.path.abspath(".")
 data_path = here + "/../../output/data/"
 latex_path = here + "/../../latex/"
-fig_path = here + "/../../output/plots/temp/"
+temp_path = here + "/../../output/plots/temp/"
+plot_path = here +"/../../output/plots/pdfs/"
 
-#   rc-params
+#   rc and plot params
 
-plt.rc("legend", fontsize=25, fancybox=True, loc="best", frameon=True, edgecolor="black")
+TICKLABELSIZE = 20
+LABELSIZE = 25
+LEGENDSIZE = 20
+TITLESIZE = 30
+
+#   Set rc-params
+
+plt.rc("legend", fontsize=LEGENDSIZE, fancybox=True, loc="best", frameon=True, edgecolor="black")
 plt.rc("font", size=25)
-plt.rc("axes", titlesize=25)
-plt.rc("xtick", labelsize=20)
-plt.rc("ytick", labelsize=20)
+plt.rc("axes", titlesize=TITLESIZE, labelsize=LABELSIZE)
+plt.rc("xtick", labelsize=TICKLABELSIZE)
+plt.rc("ytick", labelsize=TICKLABELSIZE)
+# plt.rc("tickparams")
 plt.rc("lines", linewidth=2.5)
+plt.rc('text', usetex=True)
 # plt.rc("label", fontsize=20)
 
 plt.rcParams['savefig.bbox'] = 'tight'
+plt.rcParams['font.family'] = 'Times New Roman'
+
+temp = True     # makes temporary .png files instead of .pdf
+
+#   Global setting commands
+SAVE = True
+PUSH = True
+SHOW = True
+
+def save_push(fig, pdf_name, save=SAVE, push=PUSH, show=SHOW, tight=False, png_duplicate=temp):
+    """
+    This function handles whether you want to show,
+    save and/or push the file to git.
+    Args:
+        fig (matplotlib.figure): Figure you want to handle
+        pdfname (string): Name of output pdf file
+        args (argparse)
+    """
+    if tight:
+        fig.tight_layout()
+    pdfname = pdf_name.replace('.pdf', '').strip() + ".pdf"
+    file = plot_path + pdfname
+    if save:
+        print(f'Saving plot: {file}')
+        fig.savefig(file)
+        if png_duplicate:
+            fig.savefig(temp_path + pdfname.replace('.pdf', '.png'))
+    if push:
+        os.system(f"git add {file}")
+        os.system("git commit -m 'upload plot'")
+        os.system("git push")
+    if show:
+        plt.show()
+    if not show:
+        plt.close()
+    else:
+        plt.close()
+
+
+
+def set_ax_info(ax, xlabel, ylabel=False, zlabel='none', style='plain', title=None, legend=True):
+    """Write title and labels on an axis with the correct fontsizes.
+    Args:
+        ax (matplotlib.axis): the axis on which to display information
+        title (str): the desired title on the axis
+        xlabel (str): the desired lab on the x-axis
+        ylabel (str): the desired lab on the y-axis
+    """
+    ax.set_xlabel(xlabel)
+    if ylabel != False:
+        ax.set_ylabel(ylabel)
+    if zlabel != 'none':
+        ax.set_zlabel(zlabel)
+    ax.set_title(title)
+    # ax.tick_params(axis='both', which='major', labelsize=15)
+    # ax.yaxis.get_offset_text().set_fontsize(15)
+    try:
+        ax.ticklabel_format(style=style)
+    except AttributeError:
+        pass
+    if legend:
+        ax.legend()
+    
+
+# -----------------------------------------------------------------------------
+#   General plot code above
+#   XXX
+#   Specific plot code below
+# -----------------------------------------------------------------------------
 
 
 def analytical(T):
@@ -97,38 +173,27 @@ def compare_analytical(sp, filename="anal_Nsamples4_T1.csv"):
         print(df.to_string())
 
 
-def equilibriation_time(sp):
+def equilibriation_time():
 
     e1or, m1or, n1, T1   = load("equil_L20_N100000_T1_ordered.csv")     # T=1, aligned initial spins 
-    e1un, m1un, n2, T2 = load("equil_L20_N100000_T1_unordered.csv")     # T=1, unordered (random inital spins)
+    e1un, m1un, n2, T2 = load("equil_L20_N100000_T1_unordered.csv")     # T=1, unordered (random initial spins)
     e2or, m2or, n3, T4   = load("equil_L20_N100000_T2.4_ordered.csv")   # T=2.4, aligned initial spins 
-    e2un, m2un, n4, T4 = load("equil_L20_N100000_T2.4_unordered.csv")   # T=2.4, unordered (random inital spins)
+    e2un, m2un, n4, T4 = load("equil_L20_N100000_T2.4_unordered.csv")   # T=2.4, unordered (random initial spins)
     
     ### ENERGY 
     fig1, ax1 = plt.subplots(nrows=1, ncols=2, figsize=(12,7), sharey=True)
     fig1.suptitle('Energy')
-
     ax1[0].plot(n1, e1or, '--', color='red', label='Ordered')
     ax1[0].plot(n2, e1un, ':', color='blue', label='Unordered')
     ax1[1].plot(n3, e2or, '--', color='red', label='Ordered')
     ax1[1].plot(n4, e2un, ':', color='blue', label='Unordered')
-    
     ax1[0].set_xscale('log')
     ax1[1].set_xscale('log')
     ax1[0].set_ylim(-2.1, 0)
     ax1[1].set_ylim(-2.1, 0)
-    ax1[0].legend()
-    ax1[1].legend()
-    ax1[0].set_title(r'$T=1$')
-    ax1[1].set_title(r'$T=2.4$')
-
-    ax1[0].set_ylabel(r'$\langle \epsilon \rangle$', fontsize=25)
-    ax1[0].set_xlabel(r'$N$', fontsize=25)
-    ax1[1].set_xlabel(r'$N$', fontsize=25)
-
-    if sp:
-        fig1.savefig(fig_path + "equilibriation_time_energy.png")
-        plt.close()
+    set_ax_info(ax1[0], xlabel=r'$N$', ylabel=r'$\langle \epsilon \rangle$', title=r'$T=1$')
+    set_ax_info(ax1[1], xlabel=r'$N$', title=r'$T=2.4$')
+    pdfname1 = "equilibriation_time_energy"
 
     ### MAGNETIZATION
     fig2, ax2 = plt.subplots(nrows=1, ncols=2, figsize=(12,7), sharey=True)
@@ -138,30 +203,19 @@ def equilibriation_time(sp):
     ax2[0].plot(n2, m1un, ':', color='blue', label='Unordered')
     ax2[1].plot(n3, m2or, '--', color='red', label='Ordered')
     ax2[1].plot(n4, m2un, ':', color='blue', label='Unordered')
-
     ax2[0].set_xscale('log')
     ax2[1].set_xscale('log')
     ax2[0].set_ylim(0, 1.1)
     ax2[1].set_ylim(0, 1.1)
-    ax2[0].legend()
-    ax2[1].legend()
-    ax2[0].set_title(r'$T=1$')
-    ax2[1].set_title(r'$T=2.4$')
+    set_ax_info(ax2[0], xlabel=r'$N$', ylabel=r'$\langle \vert m \vert \rangle$', title=r'$T=1$')
+    set_ax_info(ax2[1], xlabel=r'$N$', title=r'$T=2.4$')
+    pdfname2 = "equilibriation_time_magnetization"
 
-    ax2[0].set_ylabel(r'$\langle \vert m \vert \rangle$', fontsize=25)
-    ax2[0].set_xlabel(r'$N$', fontsize=25)
-    ax2[1].set_xlabel(r'$N$', fontsize=25)
+    save_push(fig1, pdfname1)
+    save_push(fig2, pdfname2)
 
 
-    if sp:
-        fig2.savefig(fig_path + "equilibriation_time_magnetization.png")
-        plt.close()
-
-    if not sp:
-        plt.show()
-
-
-def pdf_histogram(sp):
+def pdf_histogram():
    
     f1 = load("pdf_T1_unordered.csv", skiprows=0)   # (eps, abs(m), N, T)
     f2 = load("pdf_T2.4_unordered.csv", skiprows=0) # (eps, abs(m), N, T)
@@ -201,51 +255,130 @@ def pdf_histogram(sp):
     ax[0].text(0.58, 0.95, textstr1, transform=ax[0].transAxes, fontsize=20, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='white'))
     ax[1].text(0.05, 0.95, textstr2, transform=ax[1].transAxes, fontsize=20, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='white'))
 
-    if sp:
-        fig.savefig(fig_path + "histogram.png")
-        plt.close()
-
-    else:
-        plt.show()
+    pdfname = "histogram"
+    save_push(fig, pdfname)
 
 
-def PT(NT, sp=False):
-
+def PT_NT50():
     folder = data_path + "parallel/"
-    if NT==50:
-        ### (T, E, E2, abs(M), M2, var(E), var(M))
-        f1 = load("L40_nT50_NMC100000_Neq15000_para.csv", folder)
-        f2 = load("L60_nT50_NMC100000_Neq15000_para.csv", folder)
-        f3 = load("L80_nT50_NMC100000_Neq15000_para.csv", folder)
-        f4 = load("L100_nT50_NMC100000_Neq15000_para.csv", folder)
-        # FFF5 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder) # previous sim with nT=100 for comparison
-    
-    if NT==100:
-        ### (T, E, E2, abs(M), M2, var(E), var(M))
-        f1 = load("L40_nT100_NMC100000_Neq15000_para.csv", folder)
-        f2 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder)
-        f3 = load("L80_nT100_NMC100000_Neq15000_para.csv", folder)
-        f4 = load("L100_nT100_NMC100000_Neq15000_para.csv", folder)
-        # FFF5 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder) # previous sim with nT=100 for comparison
 
-    if NT==101:
-        ### (T, E, E2, abs(M), M2, var(E), var(M))
-        f1 = load("L40_nT100_NMC1000000_Neq15000_para.csv", folder)
-        f2 = load("L60_nT100_NMC1000000_Neq15000_para.csv", folder)
-        f3 = load("L80_nT100_NMC1000000_Neq15000_para.csv", folder)
-        f4 = load("L100_nT100_NMC1000000_Neq15000_para.csv", folder)
-        # FFF5 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder) # previous sim with nT=100 for comparison
+    ### (T, E, E2, abs(M), M2, var(E), var(M))
+    f1 = load("L40_nT50_NMC100000_Neq15000_para.csv", folder)
+    f2 = load("L60_nT50_NMC100000_Neq15000_para.csv", folder)
+    f3 = load("L80_nT50_NMC100000_Neq15000_para.csv", folder)
+    f4 = load("L100_nT50_NMC100000_Neq15000_para.csv", folder)
 
-        
+    files = [f1,f2,f3,f4]
+    Ls = [40, 60, 80, 100]
+    colors = ['blue', 'green', 'red', 'black', 'orange']
+
+    fig, ax = plt.subplots(1,2,figsize=(12,7))
+    for i, file in enumerate(files):
+        T = file[0]
+        varE, varM = file[-2:]
+        N = (Ls[i])**2 
+        Cv = varE / (N * T**2)
+        chi = varM / (N * T)
+
+        # cv_spline = CubicSpline(T, Cv)
+        # cv_uspline = UnivariateSpline(T, Cv)
+        # t = np.linspace(T[0], T[-1], 1000)
+        # cv_fit = cv_uspline(t)
+        # crit_idx = np.argmax(cv_fit)
+        # cv_crit = cv_fit[crit_idx]
+        # t_crit = t[crit_idx]
+        # crit_T.append(t_crit)
+        # crit_CV.append(cv_crit)
+
+        if i!=4:
+            ax[0].plot(T, Cv, 'o--', ms=4, lw=1, color=colors[i], label=f'L={Ls[i]}')
+            # ax[0].plot(t, cv_fit, '--',color=colors[i])
+            # ax[0].plot(*[t_crit, cv_crit], 'o', color='orange')
+            ax[1].plot(T, chi,'o--', ms=4, lw=1, color=colors[i], label=f'L={Ls[i]}')
+        else:
+            ax[0].plot(T, Cv,  lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
+            ax[1].plot(T, chi, lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
+
+    ax[0].set_title("Heat capacity")
+    ax[1].set_title("Susceptibility")
+
+    ax[0].set_xlabel(r'$T$')
+    ax[1].set_xlabel(r'$T$')
+
+    ax[0].set_ylabel(r'$C_V$')
+    ax[1].set_ylabel(r'$\chi$')
+
+    ax[0].legend()
+    ax[1].legend()
+
+
+    pdfname = "phase_transitionNT50"
+    save_push(fig, pdfname)
+
+def PT_NT100():
+    folder = data_path + "parallel/"
+
+    ### (T, E, E2, abs(M), M2, var(E), var(M))
+    f1 = load("L40_nT100_NMC100000_Neq15000_para.csv", folder)
+    f2 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder)
+    f3 = load("L80_nT100_NMC100000_Neq15000_para.csv", folder)
+    f4 = load("L100_nT100_NMC100000_Neq15000_para.csv", folder)
+
+    files = [f1,f2,f3,f4]
+    Ls = [40, 60, 80, 100]
+    colors = ['blue', 'green', 'red', 'black', 'orange']
+
+    fig, ax = plt.subplots(1,2,figsize=(12,7))
+    for i, file in enumerate(files):
+        T = file[0]
+        varE, varM = file[-2:]
+        N = (Ls[i])**2 
+        Cv = varE / (N * T**2)
+        chi = varM / (N * T)
+
+        if i!=4:
+            ax[0].plot(T, Cv, 'o', ms=4, lw=1, color=colors[i], label=f'L={Ls[i]}')
+            # ax[0].plot(t, cv_fit, '--',color=colors[i])
+            # ax[0].plot(*[t_crit, cv_crit], 'o', color='orange')
+            ax[1].plot(T, chi,'o', ms=4, lw=1, color=colors[i], label=f'L={Ls[i]}')
+        else:
+            ax[0].plot(T, Cv,  lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
+            ax[1].plot(T, chi, lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
+
+    ax[0].set_title("Heat capacity")
+    ax[1].set_title("Susceptibility")
+
+    ax[0].set_xlabel(r'$T$')
+    ax[1].set_xlabel(r'$T$')
+
+    ax[0].set_ylabel(r'$C_V$')
+    ax[1].set_ylabel(r'$\chi$')
+
+    ax[0].legend()
+    ax[1].legend()
+
+
+    pdfname = "phase_transitionNT100"
+    save_push(fig, pdfname)
+
+
+def PT_NT101():
+    folder = data_path + "parallel/"
+
+    ### (T, E, E2, abs(M), M2, var(E), var(M))
+    f1 = load("L40_nT100_NMC1000000_Neq15000_para.csv", folder)
+    f2 = load("L60_nT100_NMC1000000_Neq15000_para.csv", folder)
+    f3 = load("L80_nT100_NMC1000000_Neq15000_para.csv", folder)
+    f4 = load("L100_nT100_NMC1000000_Neq15000_para.csv", folder)
+
     files = [f1,f2,f3,f4]
     Ls = [40, 60, 80, 100]
     colors = ['blue', 'green', 'red', 'black', 'orange']
 
     crit_T = []
     crit_CV = []
-    
 
-    fig, ax = plt.subplots(1,2,figsize=(12,7))
+    fig1, ax1 = plt.subplots(figsize=(12,7))
     for i, file in enumerate(files):
         T = file[0]
         varE, varM = file[-2:]
@@ -264,91 +397,66 @@ def PT(NT, sp=False):
         crit_CV.append(cv_crit)
 
         if i!=4:
-            ax[0].plot(T, Cv, 'o--', ms=3, lw=0.5, color=colors[i], label=f'L={Ls[i]}')
-            ax[0].plot(t, cv_fit, '--',color=colors[i])
-            ax[0].plot(*[t_crit, cv_crit], 'o', color='orange')
-            ax[1].plot(T, chi,'o--', ms=3, lw=0.5, color=colors[i], label=f'L={Ls[i]}')
+            if i==0:
+                ax1.scatter(*[t_crit, cv_crit], marker='X', s=100, c='orange', edgecolors="black", zorder=3, label=r"$T_C$")
+            else:
+                ax1.scatter(*[t_crit, cv_crit], marker='X', s=100, c='orange', edgecolors="black", zorder=3)
+            ax1.plot(T, Cv, 'o', ms=4, lw=1, color=colors[i], zorder=1)
+            ax1.plot(t, cv_fit, '--',color=colors[i], label=f'L={Ls[i]}', zorder=2)
+
+            # ax.plot(T, chi,'.', ms=4, lw=1, color=colors[i], label=f'L={Ls[i]}')
         else:
-            ax[0].plot(T, Cv,  lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
-            ax[1].plot(T, chi, lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
+            ax1.plot(T, Cv,  lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
+            ax1.plot(T, chi, lw=3, alpha=0.5, color=colors[i], label=f'L={Ls[i]}, comparing')
 
+    ax1.set_title("Heat capacity")
 
-    ax[0].set_xlabel('Temp')
-    ax[1].set_xlabel('Temp')
+    ax1.set_xlabel(r'$T$')
 
-    ax[0].set_ylabel('Cv')
-    ax[1].set_ylabel(r'$\chi$')
+    ax1.set_ylabel(r'$C_V$')
 
-    ax[0].legend()
-    ax[1].legend()
-    if sp:
-        fig.savefig(fig_path + "phase_transition.png")
-        plt.close()
-        exit()
-    else:
-        plt.show()
-        # print('a')
-        plt.close()
+    ax1.legend()
+
+    pdfname1 = "phase_transitionNT101"
+    save_push(fig1, pdfname1)
+
+    #   Critical temperature analysis
 
     L_inv = 1/np.array(Ls)
     lreg = linregress(L_inv, crit_T)
-    L_inv_array = np.linspace(0, 0.03, 100)
+    L_inv_array = np.linspace(-0.01, 0.03, 100)
     T_c_infty = lreg.intercept 
     a = lreg.slope 
     print(lreg)
     print(f'a: {a:.4f}')
     print(f"Tc_infty={T_c_infty:.5f}")
-    plt.plot(L_inv_array, T_c_infty + a*L_inv_array )
-    plt.plot(1/np.array(Ls), crit_T, 'o')
-    plt.show()
-    exit()
-    #### OLD RESULTS WITH nT=100 
-    cv  = lambda var, L, T: var / L**2 / T**2 
-    chi = lambda var, L, T: var / L**2 / T 
 
+    linear_func = lambda xx: T_c_infty + a * xx 
 
-    T2 = load("L20_nT50_NMC100000_Neq15000_para.csv", folder, skiprows=0)[0]
-    ve2, vm2 = load("L20_nT50_NMC100000_Neq15000_para.csv", folder, skiprows=0)[-2:]
-    Cv2 = cv(ve2,20,T2)
-    chi2= cv(vm2,20,T2) 
+    fig2, ax2 = plt.subplots(figsize=(12,7))
+    ax2.plot(L_inv_array, linear_func(L_inv_array), color="blue", label="Linear fit")
+    ax2.scatter(1/np.array(Ls), crit_T, marker='X', s=100, c='orange', edgecolors="black", zorder=3, label=r"$T_C$")
+    ax2.scatter(*[0, linear_func(0)], marker='X', s=100, c='red', edgecolors="black", zorder=3, label=r"$T_C(L=\infty)$")
+    ax2.hlines(T_c_infty, xmin= -0.02, xmax=0, color="red", ls='--', lw=0.7)
+    ax2.vlines(0, ymin=np.min(linear_func(L_inv_array))-1, ymax=linear_func(0), colors='red', lw=0.7, ls='--')
+    ax2.set_xlim(L_inv_array.min(), L_inv_array.max())
+    ax2.set_ylim(linear_func(L_inv_array).min(), linear_func(L_inv_array).max())
 
-    T3, e3, e32, m3, m32, ve3, vm3 = load("L20_nT100_NMC100000_Neq15000_para.csv", folder, skiprows=0)
-    Cv3 = ve3 / 400 / T3**2 
-    chi3 = vm3 / 400 / T3 
+    ax2.set_title("Critical temperatures")
+    ax2.set_xlabel(r'$L^{-1}$')
+    ax2.set_ylabel(r'$T$')
+    ax2.legend()
 
-    T4 = load("L40_nT100_NMC100000_Neq15000_para.csv", folder, skiprows=0)[0]
-    ve4, vm4 = load("L40_nT100_NMC100000_Neq15000_para.csv", folder, skiprows=0)[-2:]
-    Cv4 = cv(ve4,40,T4)
-    chi4 = chi(vm4,40,T4)
+    pdfname2 = 'critical_temperatures'
 
-    T5 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder, skiprows=0)[0]
-    ve5, vm5 = load("L60_nT100_NMC100000_Neq15000_para.csv", folder, skiprows=0)[-2:]
-    Cv5 = cv(ve5,60,T5)
-    chi5 = chi(vm5,60,T5)
-
-    
-
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,7))
-    ax[0].plot(T2, Cv2, 'o-', ms=3, lw=1, label="L20 n50")
-    ax[0].plot(T3, Cv3, 'o-', ms=3, lw=0.5, label="L20")
-    ax[0].plot(T4, Cv4, 'o-', ms=3, lw=0.5, label="L40")
-    ax[0].plot(T5, Cv5, 'o-', ms=3, lw=0.5, label="L60")
-
-    ax[1].plot(T2, chi2, 'o-', ms=3, lw=1, label="L20 n50")
-    ax[1].plot(T3, chi3, 'o-', ms=3, lw=0.5, label="L20")
-    ax[1].plot(T4, chi4, 'o-', ms=3, lw=0.5, label="L40")
-    ax[1].plot(T5, chi5, 'o-', ms=3, lw=0.5, label="L60")
-
-    ax[0].legend()
-    ax[1].legend()
-
-    plt.show()
-
-
-sp = False # only show plots
+    save_push(fig2, pdfname2)
 
 # compare_analytical(sp)
-equilibriation_time(sp)
-pdf_histogram(sp)
-# PT(NT=100, sp=False)
+# equilibriation_time()
+# pdf_histogram()
+# PT(NT=50, sp=False)
+PT_NT50()
+PT_NT100()
+PT_NT101()
+
 
